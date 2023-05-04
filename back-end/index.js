47b3701afce384661619946d0ab41b005a53015e
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-05-04 07:53:48 +0300

    third-session

---
+++ back-end/index.js
@@ -0,0 +1,15 @@
+import expres from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(bodyParser.json({limit:"30mb", extended: "true"}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended: "true"}));
+
+app.use(cors());
+
+
+// atlas version of mongoDB
