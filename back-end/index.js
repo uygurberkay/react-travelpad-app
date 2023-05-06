c85ec12a0e1fb1f1da8b4a8ba9ba1252a0371a21
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-05-06 22:43:26 +0300

    connecting-mongodb-database

--- back-end/index.js
+++ back-end/index.js
@@ -1,4 +1,4 @@
-import expres from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -8,8 +8,17 @@ const app = express();
 
 app.use(bodyParser.json({limit:"30mb", extended: "true"}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: "true"}));
-
 app.use(cors());
 
+/*Code version of MONGODB*/
+const CONNECTION_URL = "mongodb+srv://uygurberkaytravelpad:123456Asd@cluster0.x0dcbwf.mongodb.net/"
 
-// atlas version of mongoDB
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true , useUnifiedTopology: true})
+    .then(() => {
+        app.listen(PORT,() => console.log(`Server running on port: ${PORT}`))
+    })
+    .catch((err)=> console.log(err.message));
+
+mongoose.set('useFindAndModify', false);
