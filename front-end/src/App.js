7e04c67161bc2137074a96f15945071966193f64
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-05-08 00:05:48 +0300

    mongoose-last-edition

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,30 @@
 import './css/App.css';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import TravelPad from './images/TravelPad_Logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to react-redux-boilerplate</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          TravelPad
+        </Typography>
+        <img src={TravelPad} alt="TravelPad" width={500} height={200}/>
+      </AppBar>
+      <Grow in>
+        <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+          <Grid item xs={12} sm={7}>
+            <Posts/>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form/>
+          </Grid>
+        </Grid>
+      </Grow>
+    </Container>
   );
 }
 
