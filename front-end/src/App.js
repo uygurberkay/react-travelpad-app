cfb92559a7047b37f12073169e62db717b3c4dd2
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-05-20 20:07:00 +0300

    first-styles-content-implemented

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,18 +4,19 @@ import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
 import TravelPad from './images/TravelPad_Logo.png';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
+
   return (
     <Container maxWidth='lg'>
-      <AppBar position='static' color='inherit'>
-        <Typography variant='h2' align='center'>
-          TravelPad
-        </Typography>
-        <img src={TravelPad} alt="TravelPad" width={500} height={200}/>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>TravelPad</Typography>
+        <img className={classes.image} src={TravelPad} alt="TravelPad" height={80}/>
       </AppBar>
       <Grow in>
-        <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+        <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
           <Grid item xs={12} sm={7}>
             <Posts/>
           </Grid>
--- front-end/src/components/Posts/Posts.js
+++ front-end/src/components/Posts/Posts.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Post from './Post/Post';
+import useStyles from './styles';
 
 const Posts = () => {
+  const classes = useStyles();
   return (
     <>
-        <h1>POSTS</h1>
-        <Post/>
+        <h1 className={classes}>POSTS</h1>
+        <Post />
         <Post/>
     </>
     )   
