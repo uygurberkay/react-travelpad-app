69cbaeec38bc8dbb5d46662c7750dcd30e2e20fa
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-05-20 20:40:16 +0300

    react-redux-initial-implementation

--- front-end/src/components/Posts/Post/Post.js
+++ front-end/src/components/Posts/Post/Post.js
@@ -4,7 +4,7 @@ import useStyles from './styles';
 const Post = () => {
   const classes = useStyles();
   return (
-    <div>Post</div>
+    <div className={classes}>Post</div>
   )
 }
 
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
+import {Provider} from 'react-redux';
+import {
+  createStore,
+  applyMiddleware,
+  compose} 
+from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
 import App from './App';
+
+const store = createStore(reducers,compose(applyMiddleware(thunk)))
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
