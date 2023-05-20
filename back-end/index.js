2aab7ff724c861c1ebd120523c347f8a0848f9b7
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-05-20 21:38:17 +0300

    database-issue-hot-solved

--- back-end/index.js
+++ back-end/index.js
@@ -7,12 +7,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts',postRoutes);
 
 app.use(bodyParser.json({limit:"30mb", extended: "true"}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: "true"}));
 app.use(cors());
 
+app.use('/posts',postRoutes);
 /*Code version of MONGODB*/
 const CONNECTION_URL = "mongodb+srv://uygurberkaytravelpad:123456Asd@cluster0.x0dcbwf.mongodb.net/"
 
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,27 @@
+import 
+  React, 
+  {useEffect} 
+from 'react';
 import './css/App.css';
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import {getPosts} from './actions/posts';
+
 
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
 import TravelPad from './images/TravelPad_Logo.png';
 import useStyles from './styles';
 
+
 function App() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  /* When dispatch change useEffect will render */
+  useEffect(()=> {
+    dispatch(getPosts());
+  },[dispatch])
 
   return (
     <Container maxWidth='lg'>
